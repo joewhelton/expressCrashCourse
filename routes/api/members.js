d2e110f98f522a62a2c58299cddaa8d910c58a72
Joe Whelton <joe.whelton@mycit.ie>
2020-07-05 19:11:57 +0100

    Added Members Update and Delete

--- routes/api/members.js
+++ routes/api/members.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const members = require('../../model/Members');
 const uuid = require('uuid');
 
+//Get All
 router.get('/', (req, res) => {
     res.json(members);
 });
 
+//Get by ID
 router.get('/:id', (req, res) => {
     let found = members.some(member => member.userId === parseInt(req.params.id));
     //res.send(req.params.id);
@@ -16,7 +18,6 @@ router.get('/:id', (req, res) => {
         res.status(400).json({msg: `member not found, no ID for ${req.params.id}`});
     }
 });
-
 
 // Create Member
 router.post('/', (req, res) => {
@@ -36,5 +37,45 @@ router.post('/', (req, res) => {
         res.status(200).json(members);
     }
 });
+
+//Update Member
+router.put('/:id', (req, res) => {
+    let found = members.some(member => member.userId === parseInt(req.params.id));
+    //res.send(req.params.id);
+    if(found) {
+        let updateMember = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+            emailAddress: req.body.emailAddress
+        };
+
+        members.forEach(member => {
+            if (member.userId == req.params.id){
+                member.firstName = updateMember.firstName ? updateMember.firstName : member.firstName;
+                member.lastName = updateMember.lastName ? updateMember.lastName : member.lastName;
+                member.phoneNumber = updateMember.phoneNumber ? updateMember.phoneNumber : member.phoneNumber;
+                member.emailAddress = updateMember.emailAddress ? updateMember.emailAddress : member.emailAddress;
+                res.status(200).json({ msg: "member updated", member});
+            }
+        });
+
+        //res.json(members.filter(member => member.userId === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `member not found, no ID for ${req.params.id}`});
+    }
+});
+
+//Delete by ID
+router.delete('/:id', (req, res) => {
+    let found = members.some(member => member.userId === parseInt(req.params.id));
+    //res.send(req.params.id);
+    if(found) {
+        res.json({msg: "Member deleted", members: members.filter(member => member.userId !== parseInt(req.params.id))});
+    } else {
+        res.status(400).json({msg: `member not found, no ID for ${req.params.id}`});
+    }
+});
+
 
 module.exports = router;
